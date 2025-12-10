617f2c43c802697957c6addac0f66d0e27466d2d
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-12-10 20:29:34 +0800

    feat(csv): 引入anyhow库并实现CSV数据反序列化
    - 添加anyhow依赖以增强错误处理能力
    - 定义User结构体用于CSV数据映射
    - 实现CSV文件读取与反序列化逻辑
    - 使用serde进行字段重命名映射
    - 在main函数中集成CSV子命令处理流程

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
  * @Author: MerlinSMQWQ MerlinSMQWQ@proton.me
  * @Date: 2025-12-09 23:25:13
  * @LastEditors: MerlinSMQWQ MerlinSMQWQ@proton.me
- * @LastEditTime: 2025-12-10 18:33:11
+ * @LastEditTime: 2025-12-10 20:25:12
  * @FilePath: \rust-cli-csv-processor\src\main.rs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // cli命令样例：rcli csv -i input.csv -o output.json --header --d ','
 use clap::{Parser};
+use serde::{Deserialize, Serialize};
 
 // 为结构体实现 Debug 和 Parser trait
 #[derive(Debug, Parser)]
@@ -44,6 +45,15 @@ struct  CsvOpts {
     header: bool,
 }
 
+// 定义一个数据结构用于存放CSV解析以后的数据
+#[derive(Debug, Deserialize, Serialize)]
+struct User {
+    #[serde(rename = "NackName")]
+    nackname: String,
+    #[serde(rename = "ID")]
+    id: usize,
+}
+
 // 验证输入的csv文件是否存在
 fn verify_input_file(input_file: &str) ->  Result<String, &'static str>{
     if std::path::Path::new(input_file).exists() {
@@ -56,5 +66,13 @@ fn verify_input_file(input_file: &str) ->  Result<String, &'static str>{
 
 fn main() {
     let opts = Opts::parse();
-    println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let mut reader = csv::Reader::from_path(opts.input).unwrap();
+            // csv::Reader的deserialize方法返回一个迭代器，逐行将CSV的数据反序列化为User类型，这里利用了之前定义的User结构体上的#[derive(Deserialize)]
+            // .map(|record| record.unwrap()) record是Result<User, _>类型（因为反序列化可能失败，使用unwrap()提取成功的值，忽略错误处理，将每个Result<User, _>转换为User，而map方法对迭代器中的每个元素应用一个闭包函数，将其转换为另一种类型或值。map返回一个新的迭代器，支持链式调用其他迭代器方法。
+            // .collect::<Vec<User>>() 将迭代器收集到Vec<User>向量中，最终得到完整的用户记录集合
+            let records = reader.deserialize().map(|record| record.unwrap()).collect::<Vec<User>>();
+        }
+    }
 }
